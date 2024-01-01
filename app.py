d694366a3e1fe04f3cdbce8b1bb7862bbc7cc823
Benjamin (James) van der Merwe <97449931+benjaminjvdm@users.noreply.github.com>
2024-01-01 15:07:56 +0200

    name change

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if not st.sidebar.checkbox("Hide", False):
 st.sidebar.subheader("When and where are users tweeting from?")
 hour = st.sidebar.slider("Hour to look at", 0, 23)
 modified_data = data[data['tweet_created'].dt.hour == hour]
-if not st.sidebar.checkbox("Close", True, key='sidebar-2'):
+if not st.sidebar.checkbox("Hide", True, key='sidebar-2'):
     st.markdown("### Tweet locations based on time of day")
     st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
     st.map(modified_data)
@@ -61,7 +61,7 @@ st.sidebar.subheader("Total number of tweets for each airline")
 each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='sidebar-3')
 airline_sentiment_count = data.groupby('airline')['airline_sentiment'].count().sort_values(ascending=False)
 airline_sentiment_count = pd.DataFrame({'Airline':airline_sentiment_count.index, 'Tweets':airline_sentiment_count.values.flatten()})
-if not st.sidebar.checkbox("Close", True, key='2'):
+if not st.sidebar.checkbox("Hide", True, key='2'):
     if each_airline == 'Bar plot':
         st.subheader("Total number of tweets for each airline")
         fig_1 = px.bar(airline_sentiment_count, x='Airline', y='Tweets', color='Tweets', height=500)
@@ -108,7 +108,7 @@ if len(choice) > 0:
 # Generate a word cloud from the above parameters
 st.sidebar.header("Word Cloud")
 word_sentiment = st.sidebar.radio('Display word cloud for what sentiment?', ('positive', 'neutral', 'negative'))
-if not st.sidebar.checkbox("Close", True, key='3'):
+if not st.sidebar.checkbox("Hide", True, key='3'):
     st.subheader('Word cloud for %s sentiment' % (word_sentiment))
     df = data[data['airline_sentiment']==word_sentiment]
     words = ' '.join(df['text'])
