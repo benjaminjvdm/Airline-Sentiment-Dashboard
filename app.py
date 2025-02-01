97ac16b929ab9be6ba45d2ff6d5ef5fb5201bfe9
Benjamin (James) van der Merwe <97449931+benjaminjvdm@users.noreply.github.com>
2025-02-01 19:54:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots  # Added missing import
+from plotly.subplots import make_subplots
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 from datetime import datetime
@@ -211,23 +211,23 @@ if st.sidebar.button("Download Full Dataset as CSV"):
     )
 
 st.sidebar.markdown("---")
-    st.subheader("About the Author")
+st.sidebar.subheader("About the Author")
 
-    image_url = "https://avatars.githubusercontent.com/u/97449931?v=4"
-    try:
-        response = requests.get(image_url)
-        response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
-        image = response.content
-        st.image(image, caption="Moon Benjee (문벤지)", use_container_width=True)
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error loading image: {e}")  # Use st.error for better visibility
+image_url = "https://avatars.githubusercontent.com/u/97449931?v=4"
+try:
+    response = requests.get(image_url)
+    response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+    image = response.content
+    st.sidebar.image(image, caption="Moon Benjee (문벤지)", use_container_width=True)
+except requests.exceptions.RequestException as e:
+    st.sidebar.error(f"Error loading image: {e}")  # Use st.error for better visibility
 
-    st.markdown(
-        """
-        This app was Built with ❤️ by **Benjee(문벤지)**. 
-        You can connect with me on: [LinkedIn](https://www.linkedin.com/in/benjaminjvdm/)
-        """
-    )
+st.sidebar.markdown(
+    """
+    This app was Built with ❤️ by **Benjee(문벤지)**. 
+    You can connect with me on: [LinkedIn](https://www.linkedin.com/in/benjaminjvdm/)
+    """
+)
 st.sidebar.info("""
     **Dashboard Features:**
     - Interactive date and time filters
